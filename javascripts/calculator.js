e8c061568c81d0d9f00b80c3dfe8af51c5395a62
Monica <mlee15@bayschoolsf.org>
2015-01-11 23:31:30 -0800

    Update calculator.js

--- javascripts/calculator.js
+++ javascripts/calculator.js
@@ -1,6 +1,6 @@
 // Get all the keys from document
 var keys = document.querySelectorAll('#calculator span');
-var operators = ['+', '-', 'x', '÷'];
+var operators = ['+', '-', 'x', 'Ã·'];
 var decimalAdded = false;
 
 // Add onclick event to all the keys and perform operations
@@ -23,8 +23,8 @@ for(var i = 0; i < keys.length; i++) {
 			var equation = inputVal;
 			var lastChar = equation[equation.length - 1];
 			
-			// Replace all instances of x and ÷ with * and / respectively. This can be done easily using regex and the 'g' tag which will replace all instances of the matched character/substring
-			equation = equation.replace(/x/g, '*').replace(/÷/g, '/');
+			// Replace all instances of x and Ã· with * and / respectively. This can be done easily using regex and the 'g' tag which will replace all instances of the matched character/substring
+			equation = equation.replace(/x/g, '*').replace(/Ã·/g, '/');
 			
 			// Final thing left to do is checking the last character of the equation. If it's an operator or a decimal, remove it
 			if(operators.indexOf(lastChar) > -1 || lastChar == '.')
